82559d53e452f864254a93557a1b53947551049f
Hury Gabriel <Hury_gabriel@hotmail.com>
2016-09-22 04:21:23 -0300

    Testes

--- app/src/main/java/com/projeto/sumula/sumulaeletronica/MainActivity.java
+++ app/src/main/java/com/projeto/sumula/sumulaeletronica/MainActivity.java
@@ -1,5 +1,6 @@
 package com.projeto.sumula.sumulaeletronica;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void jogador (View view){
+        Intent secondActivity = new Intent(this, Activity_Jogador.class);
+        startActivity(secondActivity);
     }
 }
