61bd8eeb207ba79f6ae483bf80af0e4f030371dd
Hury Gabriel <Hury_gabriel@hotmail.com>
2016-09-22 22:53:18 -0300

    Rebuild

---
+++ app/src/main/java/com/projeto/sumula/sumulaeletronica/ViewJogador.java
@@ -0,0 +1,16 @@
+package com.projeto.sumula.sumulaeletronica;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewJogador extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_jogador);
+
+
+
+    }
+}
