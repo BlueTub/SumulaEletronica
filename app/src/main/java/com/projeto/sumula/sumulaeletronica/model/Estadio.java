38c34d5ea22e2a29a351a6023b4402b647606336
Hury Gabriel <Hury_gabriel@hotmail.com>
2016-10-13 01:02:42 -0300

    uf estadio

--- app/src/main/java/com/projeto/sumula/sumulaeletronica/model/Estadio.java
+++ app/src/main/java/com/projeto/sumula/sumulaeletronica/model/Estadio.java
@@ -1,5 +1,7 @@
 package com.projeto.sumula.sumulaeletronica.model;
 
+import com.projeto.sumula.sumulaeletronica.enumeration.UF;
+
 import java.util.Date;
 
 /**
@@ -7,70 +9,46 @@ import java.util.Date;
  */
 
 public class Estadio {
-/*
     private String nome;
-    private String endereco;
-    private int capacidade;
-    private Date dataInauguracao;
-    private String telefone;
-    private String proprietario;
-    private String UF;
-*/
-    private String nome;
-    private String uf;
+    private UF uf;
     private int capacidade;
     private String dataInauguracao;
     private String endereco;
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public String getUf() {
+    public UF getUf() {
         return uf;
     }
-
-    public void setUf(String uf) {
+    public void setUf(UF uf) {
         this.uf = uf;
     }
-
     public int getCapacidade() {
         return capacidade;
     }
-
     public void setCapacidade(int capacidade) {
         this.capacidade = capacidade;
     }
-
     public String getDataInauguracao() {
         return dataInauguracao;
     }
-
     public void setDataInauguracao(String dataInauguracao) {
         this.dataInauguracao = dataInauguracao;
     }
-
     public String getEndereco() {
         return endereco;
     }
-
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 
     @Override
     public String toString() {
-        return "Estadio{" +
-                "nome='" + nome + '\'' +
-                ", uf='" + uf + '\'' +
-                ", capacidade=" + capacidade +
-                ", dataInauguracao='" + dataInauguracao + '\'' +
-                ", endereco='" + endereco + '\'' +
-                '}';
+        return "Estadio [nome=" + nome + ", uf=" + uf + ", capacidade=" + capacidade + ", dataInauguracao="
+                + dataInauguracao + ", endereco=" + endereco + "]";
     }
 
 }
