05c04a3f4aa721fb1519c0e6e3fe35257de3a00e
Pedro <pedropz0750@gmail.com>
2017-03-05 19:10:41 -0300

    partidas

--- app/src/main/java/com/projeto/sumula/sumulaeletronica/fragments/FragmentPartidas.java
+++ app/src/main/java/com/projeto/sumula/sumulaeletronica/fragments/FragmentPartidas.java
@@ -2,6 +2,8 @@ package com.projeto.sumula.sumulaeletronica.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,14 +21,6 @@ import com.projeto.sumula.sumulaeletronica.R;
  */
 public class FragmentPartidas extends Fragment {
 
-    private EditText etPesquisar;
-    private RadioGroup rgCampeonato, rgPesquisa;
-    private RadioButton rbData, rbClube, rbBrasilerao, rbPaulista;
-    private DatePicker datePicker;
-    private ImageButton imageButton;
-
-
-
     public FragmentPartidas() {
         // Required empty public constructor
     }
@@ -38,54 +32,18 @@ public class FragmentPartidas extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_partidas, container, false);
 
-        etPesquisar = (EditText) view.findViewById(R.id.etPesquisarPartidaClube);
-        rgCampeonato = (RadioGroup) view.findViewById(R.id.rgCampeonato);
-        rgPesquisa = (RadioGroup) view.findViewById(R.id.rgPesquisa);
-        rbData = (RadioButton) view.findViewById(R.id.rbData);
-        rbClube = (RadioButton) view.findViewById(R.id.rbClube);
-        rbBrasilerao = (RadioButton) view.findViewById(R.id.rbBrasileirao);
-        rbPaulista = (RadioButton) view.findViewById(R.id.rbPaulista);
-        datePicker = (DatePicker) view.findViewById(R.id.datePicker);
-        imageButton = (ImageButton) view.findViewById(R.id.imageButton);
-
-        rbClube.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fabSumula);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                onRadioButtonClicked(v);
+            public void onClick(View view) {
+                Snackbar.make(view, "Abrir Súmula", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
 
-        rbData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onRadioButtonClicked(v);
-            }
-        });
 
 
         return view;
-    }
-
-
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.rbClube:
-                if (checked)
-                    datePicker.setVisibility(View.INVISIBLE);
-                    etPesquisar.setVisibility(View.VISIBLE);
-                    imageButton.setVisibility(View.VISIBLE);
-                    break;
-            case R.id.rbData:
-                if (checked)
-                    datePicker.setVisibility(View.VISIBLE);
-                    etPesquisar.setVisibility(View.INVISIBLE);
-                    imageButton.setVisibility(View.VISIBLE);
-                    break;
-        }
     }
 
 }
