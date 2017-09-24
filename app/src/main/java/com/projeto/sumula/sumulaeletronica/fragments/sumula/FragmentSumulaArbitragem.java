945514d4718c350ec315deac89291bb3eaa8b92b
Pedro <pedropz0750@gmail.com>
2017-09-24 16:07:54 -0300

    sumula

--- app/src/main/java/com/projeto/sumula/sumulaeletronica/fragments/sumula/FragmentSumulaArbitragem.java
+++ app/src/main/java/com/projeto/sumula/sumulaeletronica/fragments/sumula/FragmentSumulaArbitragem.java
@@ -1,13 +1,16 @@
 package com.projeto.sumula.sumulaeletronica.fragments.sumula;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.projeto.sumula.sumulaeletronica.R;
+import com.projeto.sumula.sumulaeletronica.View.MenuLateral;
 
 import java.io.Serializable;
 
@@ -15,6 +18,9 @@ import java.io.Serializable;
  * A simple {@link Fragment} subclass.
  */
 public class FragmentSumulaArbitragem extends Fragment implements Serializable{
+
+    private ImageButton btnProximo;
+    private ImageButton btnAnterior;
 
 
     public FragmentSumulaArbitragem() {
@@ -28,7 +34,27 @@ public class FragmentSumulaArbitragem extends Fragment implements Serializable{
 
         View view =  inflater.inflate(R.layout.fragment_sumula_arbitragem, container, false);
 
+        btnAnterior = (ImageButton) view.findViewById(R.id.btnAnteriorArbitragem);
+        btnAnterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentSumulaFichaTecnica fragmentSumulaFichaTecnica = new FragmentSumulaFichaTecnica();
+                Intent i = new Intent(getActivity(), MenuLateral.class);
+                i.putExtra("fragment", fragmentSumulaFichaTecnica);
+                startActivity(i);
+            }
+        });
 
+        btnProximo= (ImageButton) view.findViewById(R.id.btnProximoArbitragem);
+        btnProximo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentSumulaCronologiaA fragmentSumulaCronologiaA = new FragmentSumulaCronologiaA();
+                Intent i = new Intent(getActivity(), MenuLateral.class);
+                i.putExtra("fragment", fragmentSumulaCronologiaA);
+                startActivity(i);
+            }
+        });
 
         return view;
     }
